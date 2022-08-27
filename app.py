ea30c4423a98612cc794bac88cb762d8f2720685
KuznetcovArtem <109390032+KuznetcovArtem@users.noreply.github.com>
2022-08-27 11:11:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def start(message: telebot.types.Message):
-    text = "Чтобы начать работу введите команду боту в следующем формате: \n <Имя валюты> <В какую валюту перевести> \
-    <Количество переводимой валюты>\nУвидеть список доступных валют: /values\nПриступить к конвертации: /convert"
+    text = "Добро пожаловать в бот-помощник расчета курсов обмена валют.\n1. Ознакомиться со списком \
+доступных валют: /values\n2. Приступить к конвертации: /convert"
     bot.send_message(message.chat.id, text)
 
 @bot.message_handler(commands=['values'])
